a677295abd45f00b2054998f6daf372f7bb58277
Faiyaj Zaman <faiyajz007@gmail.com>
2020-12-02 18:06:02 +0600

    update with Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     new MaterialApp(
-      home: new Container(
-        color: Colors.white,
-        child: Text('Alhamdulillah'),
+      home: new Center(
+        child: Column(
+          children: <Widget>[
+            Text('Alhamdulillah 1'),
+            Text('Alhamdulillah 2'),
+            Text('Alhamdulillah 3'),
+            Text('Alhamdulillah 4'),
+            Text('Alhamdulillah 5')
+          ],
+        ),
       ),
     )
   );
