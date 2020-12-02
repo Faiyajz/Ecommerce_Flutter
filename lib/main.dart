e379c997820b5bf5b568e8d5d3aa42f27a612ce3
Faiyaj Zaman <faiyajz007@gmail.com>
2020-12-02 18:17:14 +0600

    update with ListView instead of Column

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,28 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    new MaterialApp(
-      home: new Center(
-        child: Row(
+    MaterialApp(
+      home: Center(
+        child: ListView(
           children: <Widget>[
-            Text('Alhamdulillah 1'),
-            Text('Alhamdulillah 2'),
-            Text('Alhamdulillah 3'),
-            Text('Alhamdulillah 4'),
-            Text('Alhamdulillah 5')
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One'),
+            Text('One')
           ],
         ),
       ),
